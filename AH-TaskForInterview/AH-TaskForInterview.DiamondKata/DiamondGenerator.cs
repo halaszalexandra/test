5e9decb421a6533d354d7ec2b8a66ef48858aae3
halaszalexandra <53614294+halaszalexandra@users.noreply.github.com>
2022-02-28 12:08:34 +0100

    Add files via upload

---
+++ AH-TaskForInterview/AH-TaskForInterview.DiamondKata/DiamondGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AH_TaskForInterview.DiamondKata
+{
+    internal class DiamondGenerator
+    {
+        string[] diamond = new string[100];
+        int numberOfLetter = 0;
+
+        public char[] GetAlphabet()
+        {
+            char[] alphabet = new char[26]
+            {
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                'V', 'W', 'X', 'Y', 'Z'
+            };
+            return alphabet;
+        }
+        public int Search_LetterInArray(char _inputFromUser)
+        {
+            char[] alphabet = GetAlphabet();
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (alphabet[i] == _inputFromUser)
+                {
+                    numberOfLetter = i;
+                    break;
+                }
+            }
+            return numberOfLetter;
+        }
+        public string GenerateHalfDiamond(int _numberOfLetter)
+        {
+            string result= null;
+            char[] alphabet = GetAlphabet();
+            for (int i = 0; i <= _numberOfLetter; i++)
+            {
+                for (int j = 0; j < _numberOfLetter - i; j++)
+                {
+                    diamond[i] += " ";
+                }
+                diamond[i] += alphabet[i];
+
+                if (alphabet[i] != 'A')
+                {
+                    for (int j = 0; j < 2 * i - 1; j++)
+                    {
+                        diamond[i] += " ";
+                    }
+                    diamond[i] += alphabet[i];              
+                }
+                Console.WriteLine(diamond[i]);
+                result += diamond[i];
+            }
+            return result;
+        }
+        public void GenerateReverseDiamond(int _numberOfLetter)
+        {
+            for (int i = _numberOfLetter - 1; i >= 0; i--)
+            { 
+                Console.WriteLine(diamond[i]);
+            }
+        }
+    }
+}
---
+++ AH-TaskForInterview/AH-TaskForInterview.DiamondKata/DiamondGeneratorTasks.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AH_TaskForInterview.DiamondKata
+{
+    internal class DiamondGeneratorTasks
+    {
+        [Test]
+        public void B_should_give_character_sequence()
+        {
+            DiamondGenerator tasks = new DiamondGenerator();
+            string result= tasks.GenerateHalfDiamond(1);
+            Assert.AreEqual("AB", result);
+        }
+    }
+}
